10956a3895625759035abee06a5aee6e9fa45f41
softgen007 <softgen007@gmail.com>
2019-02-20 23:41:12 -0800

    initial commit

---
+++ src/main/java/demo/springboot/security/ldap/SpringSecurityLdapApplication.java
@@ -0,0 +1,12 @@
+package demo.springboot.security.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLdapApplication.class, args);
+	}
+}
