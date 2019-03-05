c561e69014455cf68c7036e796ee0bbf9418d254
softgen007 <softgen007@gmail.com>
2019-03-04 21:48:18 -0800

    updated springboot version

--- src/main/java/demo/springboot/security/ldap/config/SecurityConfiguration.java
+++ src/main/java/demo/springboot/security/ldap/config/SecurityConfiguration.java
@@ -2,11 +2,11 @@ package demo.springboot.security.ldap.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 
 import java.util.Arrays;
